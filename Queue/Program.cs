c7ebdf0380eb1760ea802a166660df7522b7d06a
vnunez94 <109703746+vnunez94@users.noreply.github.com>
2022-09-28 17:02:45 -0400

    Agreando Queue

---
+++ Queue/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Queue<T> is a generic collection that represents a first-in, first-out collection of objects.
+//https://learn.microsoft.com/es-es/dotnet/api/system.collections.generic.queue-1?view=net-6.0
+
+
+
